789ed5157dbc5d201534a57fec793725f18ade19
udeanfj <udean@figjammobile.com>
2021-08-15 18:39:47 +0200

    Front End App ToDoList Crud complete

---
+++ FrontEndApp/wwwroot/Scripts/app/RegisterCtrl.js
@@ -0,0 +1,45 @@
+﻿var app = angular.module('app');
+app.controller('RegisterController', function ($window,$http,$scope,$location, $rootScope, $stateParams, $state, LoginService) {
+    $rootScope.title = "AngularJS Login Sample";
+    $rootScope.token = "";
+    $rootScope.token_type ="";
+    $rootScope.expires_in ="";
+
+    $scope.formSubmit = function () {
+        console.log("truus" + $scope.username);
+        var loginUrl = "http://localhost/SimplicityApi/token";
+        $http.post(loginUrl,`username=${$scope.username}` +
+            `&password=${$scope.password}` +
+            '&grant_type=password'
+        ).success(function (data) {
+            var response = data;
+            console.log("truus" + response);
+            if (response != '' || response != undefined && response!='') {
+                $scope.error = '';
+                $scope.username = '';
+                $scope.password = '';
+                $rootScope.token = response.access_token;
+                $rootScope.token_type = response.token_type;
+                $rootScope.expires_in = response.expires_in;
+                $window.localStorage.setItem("token", $rootScope.token);
+                $window.localStorage.setItem("apiUrl","http://localhost/SimplicityApi/api");
+                if ($rootScope.token != '' && $rootScope.token != undefined) {
+                    console.log("truus" + LoginService.login($scope.username, $scope.password));
+                    $state.transitionTo('vehicles');
+                } else {
+                    $scope.error = "Incorrect username/password !";
+                }
+            } else {
+                $scope.error = "Incorrect username/password !";
+            }
+        }).error(function (data) {
+            console.log("truus" + data.error);
+            $scope.error = "Incorrect username/password !";
+           // $scope.error = "An Error has occured while Adding Vehicle! " + data;
+           // $scope.loading = false;
+        });
+      
+
+       
+    };
+});
---
+++ FrontEndApp/wwwroot/Scripts/app/app.js
@@ -0,0 +1,39 @@
+﻿var app = angular.module('app', ['ui.router', 'ngIdle', 'ngSanitize', 'ui.bootstrap',]);
+console.log(window.location.origin);
+angular.module('app').config(['KeepaliveProvider', 'IdleProvider', function (KeepaliveProvider, IdleProvider) {
+    // configure Idle settings
+    IdleProvider.idle(60 * 60);
+    IdleProvider.timeout(5);
+    KeepaliveProvider.interval(10);
+    IdleProvider.interrupt('keydown wheel mousedown touchstart touchmove scroll');
+}]);
+app.run(function ($rootScope, $location, $state, LoginService) {
+    console.clear();
+    console.log('running');
+    if (!LoginService.isAuthenticated()) {
+        console.log('no login');
+        $state.transitionTo('login');
+    }
+});
+app.config(['$stateProvider', '$urlRouterProvider', '$locationProvider',
+    function ($stateProvider, $urlRouterProvider,$locationProvider) {
+        var baseUrl = "https://localhost:44399/";
+        $stateProvider
+            .state('login', {
+                url: '/login',
+                templateUrl:'/Login',
+                controller: 'LoginController'
+            })
+            .state('register', {
+                url: '/register',
+                templateUrl:'/Register',
+                controller: 'RegisterController'
+            })
+            .state('todolists', {
+                url: '/todolists',
+                templateUrl: '/ToDoList',
+                controller: 'ToDoListController'
+            });
+     
+        $urlRouterProvider.otherwise('/login');
+    }]);
